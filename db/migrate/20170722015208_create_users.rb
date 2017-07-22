ffdefd593631cf415551d7ea8c44adbda1cdac95
laura <lmelgarejos@unal.edu.co>
2017-07-21 21:04:22 -0700

    changed username attr in user model

--- db/migrate/20170722015208_create_users.rb
+++ db/migrate/20170722015208_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :login
-      t.string :username
+      t.string :name
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -80,7 +80,7 @@ ActiveRecord::Schema.define(version: 20170722015208) do
 
   create_table "users", force: :cascade do |t|
     t.string "login"
-    t.string "username"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
