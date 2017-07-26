5165cacd9883d48e4630cf0095afb27b0c00b25a
laura <lmelgarejos@unal.edu.co>
2017-07-26 11:09:25 -0700

    Working with forntend

--- app/serializers/driver_serializer.rb
+++ app/serializers/driver_serializer.rb
@@ -1,5 +1,5 @@
 class DriverSerializer < ActiveModel::Serializer
-  attributes :id, :username, :first_name, :last_name, :home_address, :home_zip_code, :work_address, :work_zip_code, :license_number, :email, :phone_number, :user_id
+  attributes :id, :username, :user_id#, :first_name, :last_name, :home_address, :home_zip_code, :work_address, :work_zip_code, :license_number, :email, :phone_number, :user_id
 
   has_many :trips
   has_many :cars
